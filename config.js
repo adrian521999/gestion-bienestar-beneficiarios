19525b6865dcf1738941d8ef0ee375f5a4e38d02
adrian521999 <adriancondori2199@gmail.com>
2026-06-26 12:26:24 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-window.SUCCOR_CONFIG = {
+// Configuración del proyecto Supabase
+// 1) Pega aquí tu URL de proyecto, SIN /rest/v1 al final.
+// 2) Pega tu anon public key desde Supabase > Project Settings > API.
+window.APP_CONFIG = {
   SUPABASE_URL: "https://vahzjuxegbzxielfmdri.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_hmesfGFMfElmFRA_1UgCRA_B-zbH90A"
 };
